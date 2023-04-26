f7a7188d861feda5e6e7850ab6fbe1339efee0b0
Ramiz Ali <ramiz.apk7@gmail.com>
2023-04-26 07:35:51 +0300

    complated assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,15 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(`before setState: ${count}`);
+
     setCount(count + 1);
+
+    console.log(`after setState: ${count}`);
+
+    // call setCount twice, to update the counter by two every time we click
+    // setCount(count + 1);
+    // setCount(count + 1);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
